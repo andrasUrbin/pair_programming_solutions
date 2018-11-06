0c664d41a2605abc10096b03ab50280cfea293a5
András Urbin <urbinandras@gmail.com>
2018-11-06 10:26:51 +0100

    Everything done

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,21 @@
 def fizzbuzz(number):
-    return
+    if number % 3 == 0 and number % 5 == 0:
+        number = "FizzBuzz"
+        return number
+    elif number % 3 == 0:
+        number = "Fizz"
+        return number
+    elif number % 5 == 0:
+        number = "Buzz"
+        return number
+    else:
+        return number
 
 
 def main():
-    return
+    for number in range(1, 101):
+        print(fizzbuzz(number))
+
 
 if __name__ == '__main__':
     main()
--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,18 @@
 def palindrome(str):
-    return
+    bString = str.strip().lower().replace(" ", "")
+    cString = bString[::-1]
+    print(cString)
+    if bString == cString:
+        print("It's a palindrome!")
+        return True
+    else:
+        print("It's not a palindrome!")
+        return False
 
 
 def main():
-    return
+    aString = input("What's your word?")
+    palindrome(aString)
 
 
 if __name__ == '__main__':
